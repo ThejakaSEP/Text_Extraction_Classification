02ef1dcba08ee2817e0f00c161258d3bd6fa6350
ThejakaSEP <thejaka.mahaulpatha@gmail.com>
2023-03-18 02:32:05 -0400

    Interface

--- file_iterator.py
+++ file_iterator.py
@@ -4,31 +4,39 @@
 import os
 from image_extractor import ImageExtractor
 
-
-folder_path = '/Users/thejakamahaulpatha/Desktop/Break/Documents'
+folder_path_sample = '/Users/thejakamahaulpatha/Desktop/Break/Documents'
 
 # iterate over files in
 # that directory
-for filename in os.listdir(folder_path):
-    f = os.path.join(folder_path, filename)
 
-    obj_1 = ImageExtractor()
+class FileIterator:
+    def __init__(self,folder_path):
+        self.folder_path = folder_path
 
-    if f.endswith('.jpg'):
-        path_to_image = f
-        extracted_text = obj_1.extract_text_from_image(path_to_image)
-        preprocessed_text = obj_1.preprocess_text(extracted_text)
-        file_class = obj_1.classifier(preprocessed_text)
-        print(f"File Class : {file_class}")
-        # Copy the file to relevant folder
-        obj_1.move_file(path_to_image,folder_path,file_class)
+    def iterate_files(self):
 
-    elif f.endswith('.pdf'):
-        path_to_pdf = f
-        extracted_text_pdf = obj_1.extract_text_from_pdf(path_to_pdf)
-        preprocessed_text_pdf = obj_1.preprocess_text(extracted_text_pdf)
-        file_class_pdf = obj_1.classifier(preprocessed_text_pdf)
-        print(f"File Class : {file_class_pdf}")
-        # Copy the file to relevant folder
-        obj_1.move_file(path_to_pdf,folder_path,file_class_pdf)
+        for filename in os.listdir(self.folder_path):
+            f = os.path.join(self.folder_path, filename)
 
+            obj_1 = ImageExtractor()
+
+            if f.endswith('.jpg') or f.endswith('.png'):
+                path_to_image = f
+                extracted_text = obj_1.extract_text_from_image(path_to_image)
+                preprocessed_text = obj_1.preprocess_text(extracted_text)
+                file_class = obj_1.classifier(preprocessed_text)
+                print(f"File Class : {file_class}")
+                # Copy the file to relevant folder
+                obj_1.move_file(path_to_image,self.folder_path,file_class)
+
+            elif f.endswith('.pdf'):
+                path_to_pdf = f
+                extracted_text_pdf = obj_1.extract_text_from_pdf(path_to_pdf)
+                preprocessed_text_pdf = obj_1.preprocess_text(extracted_text_pdf)
+                file_class_pdf = obj_1.classifier(preprocessed_text_pdf)
+                print(f"File Class : {file_class_pdf}")
+                # Copy the file to relevant folder
+                obj_1.move_file(path_to_pdf,self.folder_path,file_class_pdf)
+
+# obj2 = FileIterator(folder_path_sample)
+# obj2.iterate_files()
